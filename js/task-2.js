69aa9f090c518189d529ddb0f5cdd7ff932682ef
WebAlex2003 <nikolenkolesha30032003@gmail.com>
2024-04-04 00:50:19 +0300

    class-get-set

---
+++ js/task-2.js
@@ -0,0 +1,28 @@
+/* Task 2 */
+
+class Storage {
+  constructor(newItems) {
+    this.items = newItems;
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  addItem(newItem) {
+    this.items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    this.items = this.items.filter((item) => item !== itemToRemove);
+  }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
